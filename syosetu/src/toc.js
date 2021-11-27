d07d39b220bfdc51501460fed9e41aa185a323d4
Beast666 <khoapronk@gmail.com>
2021-11-27 19:56:34 +0700

    Fix syosetu's oneshot error

--- syosetu/src/toc.js
+++ syosetu/src/toc.js
@@ -2,14 +2,21 @@ function execute(url) {
     const doc = Http.get(url).html();
     var allChap = doc.select(".novel_sublist2 a")
     const data = [];
-    for (var i = 0;i < allChap.size(); i++) {
-        var chap = allChap.get(i);
+    if (allChap) {
+        for (var i = 0;i < allChap.size(); i++) {
+            var chap = allChap.get(i);
+            data.push({
+                name: chap.select("a").text(),
+                url: chap.attr("href"),
+                host: "http://ncode.syosetu.com"
+            })
+        }
+    } else {
         data.push({
-            name: chap.select("a").text(),
-            url: chap.attr("href"),
-            host: "http://ncode.syosetu.com"
+            name: doc.select(".novel_title").text(),
+            url: url,
+            host: ""
         })
     }
-
     return Response.success(data);
 }
