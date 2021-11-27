b58fe6abf18eeb625364e218fc6d1afb42560044
Beast666 <khoapronk@gmail.com>
2021-11-27 21:44:10 +0700

    fix syosetu's oneshot error

--- syosetu/src/search.js
+++ syosetu/src/search.js
@@ -1,25 +1,26 @@
 function execute(key, page) {
-    if (!page) page = '1';
+  if (!page) page = '1';
 
-    var doc = Http.get("https://yomou.syosetu.com/search.php?&search_type=novel&order_former=search&order=new&notnizi=1").params({word: key, p: page}).html();
-    if (doc) {
-        var el = doc.select(".searchkekka_box");
-        var novelList = [];
-        var next = doc.select(".pager b +a").text()
+  var doc = Http.get('https://yomou.syosetu.com/search.php?&search_type=novel&order_former=search&order=new&notnizi=1')
+    .params({ word: key, p: page })
+    .html();
+  if (doc) {
+    var el = doc.select('.searchkekka_box');
+    var novelList = [];
+    var next = doc.select('.pager b +a').text();
 
-        for (var i = 0; i < el.size(); i++) {
-            var e = el.get(i);
-            novelList.push({
-                name: e.select(".novel_h a").text(),
-                link: e.select(".novel_h a").attr("href"),
-                description: e.select(".left").text(),
-                cover: 'https://raw.githubusercontent.com/khoa301020/vbook-ext/master/syosetu/not_found.png',
-                host: ""
-            });
-
-        }
-
-        return Response.success(novelList, next);
+    for (var i = 0; i < el.size(); i++) {
+      var e = el.get(i);
+      novelList.push({
+        name: e.select('.novel_h a').text(),
+        link: e.select('.novel_h a').attr('href'),
+        description: e.select('.left').text(),
+        cover: 'https://raw.githubusercontent.com/khoa301020/vbook-ext/master/syosetu/not_found.png',
+        host: '',
+      });
     }
-    return null;
+
+    return Response.success(novelList, next);
+  }
+  return null;
 }
--- syosetu/src/toc.js
+++ syosetu/src/toc.js
@@ -1,22 +1,21 @@
 function execute(url) {
-    const doc = Http.get(url).html();
-    var allChap = doc.select(".novel_sublist2 a")
-    const data = [];
-    if (allChap) {
-        for (var i = 0;i < allChap.size(); i++) {
-            var chap = allChap.get(i);
-            data.push({
-                name: chap.select("a").text(),
-                url: chap.attr("href"),
-                host: "http://ncode.syosetu.com"
-            })
-        }
-    } else {
-        data.push({
-            name: doc.select(".novel_title").text(),
-            url: url,
-            host: ""
-        })
-    }
-    return Response.success(data);
+  const doc = Http.get(url).html();
+  var allChap = doc.select('.novel_sublist2 a');
+  const data = [];
+  for (var i = 0; i < allChap.size(); i++) {
+    var chap = allChap.get(i);
+    data.push({
+      name: chap.select('a').text(),
+      url: chap.attr('href'),
+      host: 'http://ncode.syosetu.com',
+    });
+  }
+  if (data.length == 0) {
+    data.push({
+      name: doc.select('.novel_title').text(),
+      url: url,
+      host: '',
+    });
+  }
+  return Response.success(data);
 }
