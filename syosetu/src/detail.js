84e8417c0acc600e9495031f16f62022f6a2b9f4
Beast666 <khoapronk@gmail.com>
2021-11-27 20:08:57 +0700

    fix syosetu's detail.js

--- syosetu/src/detail.js
+++ syosetu/src/detail.js
@@ -1,13 +1,16 @@
 function execute(url) {
   var doc = Http.get(url).html();
-  if (doc) {    
-
+  if (doc) {
     return Response.success({
       name: doc.select('.series-name').text(),
       cover: 'https://cdn.discordapp.com/attachments/694132681994272778/914069250262569000/default.png',
       host: 'https://ncode.syosetu.com',
       author: doc.select('.novel_writername a').text(),
       description: doc.select('div#novel_ex').html(),
+      detail: "",
+      ongoing: ""
     });
+  } else {
+    alert('Error');
   }
 }
