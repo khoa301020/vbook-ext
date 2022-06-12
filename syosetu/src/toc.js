214ab66c475ec3bd86b77cb4e93d33584b9f1cd2
Beast666 <khoapronk@gmail.com>
2022-06-12 21:01:42 +0700

    Upgrade syosetu v5: Add chapter name

--- syosetu/src/toc.js
+++ syosetu/src/toc.js
@@ -1,21 +1,36 @@
 function execute(url) {
-  const doc = fetch(url).html();
-  var allChap = doc.select('.novel_sublist2 a');
-  const data = [];
-  for (var i = 0; i < allChap.size(); i++) {
-    var chap = allChap.get(i);
-    data.push({
-      name: chap.select('a').text(),
-      url: chap.attr('href'),
-      host: 'http://ncode.syosetu.com',
-    });
-  }
-  if (data.length == 0) {
-    data.push({
-      name: '「短編」・「' + doc.select('.novel_title').text() + '」',
-      url: url,
-      host: '',
-    });
-  }
-  return Response.success(data);
+    const doc = fetch(url).html();
+    var allChap = doc.select('.novel_sublist2 a,.chapter_title');
+    const data = [];
+    var chapterName = ""
+    // for (var i = 0; i < allChap.size(); i++) {
+    //     var chap = allChap.get(i);
+
+    //     data.push({
+    //         name: chap.select('a').text(),
+    //         url: chap.attr('href'),
+    //         host: 'http://ncode.syosetu.com',
+    //     });
+    // }
+    for (var i = 0; i < allChap.size(); i++) {
+        var chap = allChap.get(i);
+        if (chap.select('a').text() == "") {
+            chapterName = "【" + chap.text() + "】\n\n";
+        } else {
+            data.push({
+                name: chapterName + chap.select('a').text(),
+                url: chap.attr('href'),
+                host: 'http://ncode.syosetu.com',
+            });
+            chapterName = "";
+        }
+    }
+    if (data.length == 0) {
+        data.push({
+            name: '「短編」・「' + doc.select('.novel_title').text() + '」',
+            url: url,
+            host: '',
+        });
+    }
+    return Response.success(data);
 }
